ade08b86f79d03393838ddbe433d2730732d7ce6
Muhammed Salih <zalih@mail.com>
2016-05-31 10:09:59 +0530

    with limiting the bounds

--- ScaleImageView.java
+++ ScaleImageView.java
@@ -1,214 +1,226 @@
 package com.mz.imagezoomview;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
 import android.graphics.Matrix;
-import android.graphics.Paint;
-import android.graphics.drawable.BitmapDrawable;
+import android.graphics.PointF;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
+import android.view.View;
 import android.widget.ImageView;
 
 public class ScaleImageView extends ImageView {
-	private Paint borderPaint = null;
-	private Paint backgroundPaint = null;
-
-	private float mPosX = 0f;
-	private float mPosY = 0f;
-
-	private float mLastTouchX;
-	private float mLastTouchY;
-	private static final int INVALID_POINTER_ID = -1;
-	private static final String LOG_TAG = "TouchImageView";
-	ScaleGestureDetector mScaleDetector;
-	float    mScaleFactor;
-	// The ‘active pointer’ is the one currently moving our object.
-	private int mActivePointerId = INVALID_POINTER_ID;
-
-
-    public ScaleImageView(Context context, AttributeSet attr) {
-        super(context, attr);
-        reset();
-       }
-
+	
+	
+	
+	Matrix matrix;
+	 
+    // We can be in one of these 3 states
+    static final int NONE = 0;
+    static final int DRAG = 1;
+    static final int ZOOM = 2;
+    int mode = NONE;
+ 
+    // Remember some things for zooming
+    PointF last = new PointF();
+    PointF start = new PointF();
+    float minScale = 1f;
+    float maxScale = 3f;
+    float[] m;
+ 
+    int viewWidth, viewHeight;
+    static final int CLICK = 3;
+    float saveScale = 1f;
+    protected float origWidth, origHeight;
+    int oldMeasuredWidth, oldMeasuredHeight;
+ 
+    ScaleGestureDetector mScaleDetector;
+ 
+    Context context;
+ 
     public ScaleImageView(Context context) {
         super(context);
-        reset();
-      }
-
-    @Override
-    public void setImageBitmap(Bitmap bm) {
-        super.setImageBitmap(bm);
-      }
-
-    @Override
-    public void setImageResource(int resId) {
-        super.setImageResource(resId);
-     }
-    public void reset(){
-    	mScaleDetector = new ScaleGestureDetector(getContext(), new ScaleListener());
-
-        borderPaint = new Paint();
-        borderPaint.setARGB(255, 255, 128, 0);
-        borderPaint.setStyle(Paint.Style.STROKE);
-        borderPaint.setStrokeWidth(4);
-
-        backgroundPaint = new Paint();
-        backgroundPaint.setARGB(32, 255, 255, 255);
-        backgroundPaint.setStyle(Paint.Style.FILL);
-
-    	
+        sharedConstructing(context);
     }
-    @Override
-    public boolean onTouchEvent(MotionEvent ev) {
-        // Let the ScaleGestureDetector inspect all events.
-        mScaleDetector.onTouchEvent(ev);
-
-        final int action = ev.getAction();
-        switch (action & MotionEvent.ACTION_MASK) {
-        case MotionEvent.ACTION_DOWN: {
-            final float x = ev.getX();
-            final float y = ev.getY();
-
-            mLastTouchX = x;
-            mLastTouchY = y;
-
-            mActivePointerId = ev.getPointerId(0);
-            break;
-        }
-
-        case MotionEvent.ACTION_MOVE: {
-            final int pointerIndex = ev.findPointerIndex(mActivePointerId);
-            final float x = ev.getX(pointerIndex);
-            final float y = ev.getY(pointerIndex);
-
-            // Only move if the ScaleGestureDetector isn't processing a gesture.
-            if (!mScaleDetector.isInProgress()) {
-                final float dx = x - mLastTouchX;
-                final float dy = y - mLastTouchY;
-
-                mPosX += dx;
-                mPosY += dy;
-
+ 
+    public ScaleImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        sharedConstructing(context);
+    }
+    
+    private void sharedConstructing(Context context) {
+        super.setClickable(true);
+        this.context = context;
+        mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
+        matrix = new Matrix();
+        m = new float[9];
+        setImageMatrix(matrix);
+        setScaleType(ScaleType.MATRIX);
+ 
+        setOnTouchListener(new OnTouchListener() {
+ 
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                mScaleDetector.onTouchEvent(event);
+                PointF curr = new PointF(event.getX(), event.getY());
+ 
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                       last.set(curr);
+                        start.set(last);
+                        mode = DRAG;
+                        break;
+                        
+                    case MotionEvent.ACTION_MOVE:
+                        if (mode == DRAG) {
+                            float deltaX = curr.x - last.x;
+                            float deltaY = curr.y - last.y;
+                            float fixTransX = getFixDragTrans(deltaX, viewWidth, origWidth * saveScale);
+                            float fixTransY = getFixDragTrans(deltaY, viewHeight, origHeight * saveScale);
+                            matrix.postTranslate(fixTransX, fixTransY);
+                            fixTrans();
+                            last.set(curr.x, curr.y);
+                        }
+                        break;
+ 
+                    case MotionEvent.ACTION_UP:
+                        mode = NONE;
+                        int xDiff = (int) Math.abs(curr.x - start.x);
+                        int yDiff = (int) Math.abs(curr.y - start.y);
+                        if (xDiff < CLICK && yDiff < CLICK)
+                            performClick();
+                        break;
+ 
+                    case MotionEvent.ACTION_POINTER_UP:
+                        mode = NONE;
+                        break;
+                }
+                
+                setImageMatrix(matrix);
                 invalidate();
+                return true; // indicate event was handled
             }
-
-            mLastTouchX = x;
-            mLastTouchY = y;
-            break;
-        }
-
-        case MotionEvent.ACTION_UP: {
-            mActivePointerId = INVALID_POINTER_ID;
-            break;
-        }
-
-        case MotionEvent.ACTION_CANCEL: {
-            mActivePointerId = INVALID_POINTER_ID;
-            break;
-        }
-
-        case MotionEvent.ACTION_POINTER_UP: {
-            final int pointerIndex = (ev.getAction() & MotionEvent.ACTION_POINTER_INDEX_MASK) >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
-            final int pointerId = ev.getPointerId(pointerIndex);
-            if (pointerId == mActivePointerId) {
-                // This was our active pointer going up. Choose a new
-                // active pointer and adjust accordingly.
-                final int newPointerIndex = pointerIndex == 0 ? 1 : 0;
-                mLastTouchX = ev.getX(newPointerIndex);
-                mLastTouchY = ev.getY(newPointerIndex);
-                mActivePointerId = ev.getPointerId(newPointerIndex);
-            }
-            break;
-        }
-        }
-
-        return true;
+ 
+        });
     }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see android.view.View#draw(android.graphics.Canvas)
-     */
-    @Override
-    public void draw(Canvas canvas) {
-        super.draw(canvas);
-        canvas.drawRect(0, 0, getWidth() - 1, getHeight() - 1, borderPaint);
+ 
+    public void setMaxZoom(float x) {
+        maxScale = x;
     }
-
-    @Override
-    public void onDraw(Canvas canvas) {
-        canvas.drawRect(0, 0, getWidth() - 1, getHeight() - 1, backgroundPaint);
-        if (this.getDrawable() != null) {
-            canvas.save();
-            canvas.translate(mPosX, mPosY);
-
-            Matrix matrix = new Matrix();
-            matrix.postScale(mScaleFactor, mScaleFactor, pivotPointX,
-                    pivotPointY);
-            // canvas.setMatrix(matrix);
-
-            canvas.drawBitmap(
-                    ((BitmapDrawable) this.getDrawable()).getBitmap(), matrix,
-                    null);
-
-            // this.getDrawable().draw(canvas);
-            canvas.restore();
-        }
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * android.widget.ImageView#setImageDrawable(android.graphics.drawable.Drawable
-     * )
-     */
-    @Override
-    public void setImageDrawable(Drawable drawable) {
-        // Constrain to given size but keep aspect ratio
-        int width = drawable.getIntrinsicWidth();
-        int height = drawable.getIntrinsicHeight();
-        mLastTouchX = mPosX = 0;
-        mLastTouchY = mPosY = 0;
-
-        int borderWidth = (int) borderPaint.getStrokeWidth();
-        mScaleFactor = Math.min(((float) getLayoutParams().width - borderWidth)
-                / width, ((float) getLayoutParams().height - borderWidth)
-                / height);
-        pivotPointX = (((float) getLayoutParams().width - borderWidth) - (int) (width * mScaleFactor)) / 2;
-        pivotPointY = (((float) getLayoutParams().height - borderWidth) - (int) (height * mScaleFactor)) / 2;
-        super.setImageDrawable(drawable);
-    }
-
-    float pivotPointX = 0f;
-    float pivotPointY = 0f;
-
-    private class ScaleListener extends
-            ScaleGestureDetector.SimpleOnScaleGestureListener {
-
+ 
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
         @Override
-        public boolean onScale(ScaleGestureDetector detector) {
-            mScaleFactor *= detector.getScaleFactor();
-
-            pivotPointX = detector.getFocusX();
-            pivotPointY = detector.getFocusY();
-
-            Log.d(LOG_TAG, "mScaleFactor " + mScaleFactor);
-            Log.d(LOG_TAG, "pivotPointY " + pivotPointY + ", pivotPointX= "
-                    + pivotPointX);
-            mScaleFactor = Math.max(0.05f, mScaleFactor);
-
-            invalidate();
+        public boolean onScaleBegin(ScaleGestureDetector detector) {
+            mode = ZOOM;
             return true;
         }
-
-
+ 
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            float mScaleFactor = detector.getScaleFactor();
+            float origScale = saveScale;
+            saveScale *= mScaleFactor;
+            if (saveScale > maxScale) {
+                saveScale = maxScale;
+                mScaleFactor = maxScale / origScale;
+            } else if (saveScale < minScale) {
+                saveScale = minScale;
+                mScaleFactor = minScale / origScale;
+            }
+ 
+            if (origWidth * saveScale <= viewWidth || origHeight * saveScale <= viewHeight)
+                matrix.postScale(mScaleFactor, mScaleFactor, viewWidth / 2, viewHeight / 2);
+            else
+                matrix.postScale(mScaleFactor, mScaleFactor, detector.getFocusX(), detector.getFocusY());
+ 
+            fixTrans();
+            return true;
+        }
+    }
+ 
+    void fixTrans() {
+        matrix.getValues(m);
+        float transX = m[Matrix.MTRANS_X];
+        float transY = m[Matrix.MTRANS_Y];
+        
+        float fixTransX = getFixTrans(transX, viewWidth, origWidth * saveScale);
+        float fixTransY = getFixTrans(transY, viewHeight, origHeight * saveScale);
+ 
+        if (fixTransX != 0 || fixTransY != 0)
+            matrix.postTranslate(fixTransX, fixTransY);
+    }
+ 
+    float getFixTrans(float trans, float viewSize, float contentSize) {
+        float minTrans, maxTrans;
+ 
+        if (contentSize <= viewSize) {
+            minTrans = 0;
+            maxTrans = viewSize - contentSize;
+        } else {
+            minTrans = viewSize - contentSize;
+            maxTrans = 0;
+        }
+ 
+        if (trans < minTrans)
+            return -trans + minTrans;
+        if (trans > maxTrans)
+            return -trans + maxTrans;
+        return 0;
+    }
     
-}
+    float getFixDragTrans(float delta, float viewSize, float contentSize) {
+        if (contentSize <= viewSize) {
+            return 0;
+        }
+        return delta;
+    }
+ 
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        viewWidth = MeasureSpec.getSize(widthMeasureSpec);
+        viewHeight = MeasureSpec.getSize(heightMeasureSpec);
+        
+        //
+        // Rescales image on rotation
+        //
+        if (oldMeasuredHeight == viewWidth && oldMeasuredHeight == viewHeight
+                || viewWidth == 0 || viewHeight == 0)
+            return;
+        oldMeasuredHeight = viewHeight;
+        oldMeasuredWidth = viewWidth;
+ 
+        if (saveScale == 1) {
+            //Fit to screen.
+            float scale;
+ 
+            Drawable drawable = getDrawable();
+            if (drawable == null || drawable.getIntrinsicWidth() == 0 || drawable.getIntrinsicHeight() == 0)
+                return;
+            int bmWidth = drawable.getIntrinsicWidth();
+            int bmHeight = drawable.getIntrinsicHeight();
+            
+            Log.d("bmSize", "bmWidth: " + bmWidth + " bmHeight : " + bmHeight);
+ 
+            float scaleX = (float) viewWidth / (float) bmWidth;
+            float scaleY = (float) viewHeight / (float) bmHeight;
+            scale = Math.min(scaleX, scaleY);
+            matrix.setScale(scale, scale);
+ 
+            // Center the image
+            float redundantYSpace = (float) viewHeight - (scale * (float) bmHeight);
+            float redundantXSpace = (float) viewWidth - (scale * (float) bmWidth);
+            redundantYSpace /= (float) 2;
+            redundantXSpace /= (float) 2;
+ 
+            matrix.postTranslate(redundantXSpace, redundantYSpace);
+ 
+            origWidth = viewWidth - 2 * redundantXSpace;
+            origHeight = viewHeight - 2 * redundantYSpace;
+            setImageMatrix(matrix);
+        }
+        fixTrans();
+    }
 }
